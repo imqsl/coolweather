872f57fcad1da98334ba2d13cc694f2e167b6751
imqsl <imqsl@163.com>
2017-05-23 20:53:37 +0800

    增加后台自动更细那天起的功能

--- app/src/main/java/com/imqsl/coolweather/WeatherActivity.java
+++ app/src/main/java/com/imqsl/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.imqsl.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -208,5 +209,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+
     }
 }
